b5b5917df2c9dbeb6f7a294ac45ce4485f9a09cf
Avengers <talha.tech01@gmail.com>
2022-10-23 18:42:37 +0500

    Setup Go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main (){
+	const companyName = "FiveRivers Technologies"
+	const totalBookings = 50
+	var remainingBookings = totalBookings
+	fmt.Printf("Welcom To %v \n", companyName)
+	fmt.Printf("We have %v booking slots available out of %v \n\n", remainingBookings, totalBookings)
+	var customerName string
+	var selectedSlots int
+	customerName = "Talha Muneer"
+	selectedSlots = 4
+	fmt.Printf("Hi %v,\nYou've selected %v Slots.\n", customerName, selectedSlots)
+
+}
